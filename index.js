93de0658d619e9fbd6da150559cb0fb4002f3ea2
Bob Brown <robertbernardbrown@gmail.com>
2018-06-02 16:42:10 -0400

    creates an echo bot

--- index.js
+++ index.js
@@ -34,3 +34,24 @@ app.post('/webhook', (req, res) => {
         res.status(200).end();
     }
 }); 
+
+function sendMessage(event) {
+    let sender = event.sender.id;
+    let text = event.message.text;
+    console.log(event);
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token: 'EAAGxKtyyOWQBAJSnZB7ppKntJvYiZC3kxH8P7bTwa7kYXD7ujZC0g76VwKI2AIGW7Cl88h4yO9jZBx1i1ZCfZAR2IO5ZCLef3zRBPtrn2WMHOFBfjB3ZAsFmGR80IXZAYDnsZB1tYRdfuNmahFHAGYABUGN7CyZCNiEog4dvTaah5UeXwZDZD'},
+      method: 'POST',
+      json: {
+        recipient: {id: sender},
+        message: {text: text}
+      }
+    }, function (error, response) {
+      if (error) {
+          console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error);
+      }
+    });
+  }
